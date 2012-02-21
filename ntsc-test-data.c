58935a6f0c26442ce8f1b5caf03bcd2ca97c6254
Christopher Abad <aempirei@gmail.com>
2012-02-21 08:45:35 -0800

    io

--- ntsc-test-data.c
+++ ntsc-test-data.c
@@ -22,6 +22,19 @@
 const int frame_scanlines = 525;
 const float us_epsilon = 0.25;
 
+const int field_scanlines = 240;
+
+const float scanline_us = 63.50;
+
+const float hsync_front_porch_us = 1.50;
+const float hsync_sync_tip_us    = 4.75;
+const float hsync_breezeway_us   = 0.50;
+const float hsync_color_burst_us = 2.75;
+const float hsync_back_porch_us  = 1.50;
+
+float hsync_us;
+float hdata_us;
+
 float *vsync(); // 9 scanlines
 float *junk(); // 0.5 @ Vblk
 float *vblank(); // 13 X vblank_scanline() ; 13 X [ 11us @ HSYNC ][ 52.5us @ Vblk ]
@@ -40,29 +53,25 @@ float *hdata(unsigned char *); // 52.5us @ f(t)V
 
 float *hsync(); //  [ 11us @ HSYNC ] := [ 1.5us @ Vblk ][ 4.75us @ 0V ][ 0.5us @ Vblk ][ 2.75us @ Vblk ][ 1.5us @ Vblk ]
 
-const int field_data_scanlines = 240;
-
-const float scanline_us = 63.50;
-
-const float hsync_front_porch_us = 1.50;
-const float hsync_sync_tip_us    = 4.75;
-const float hsync_breezeway_us   = 0.50;
-const float hsync_color_burst_us = 2.75;
-const float hsync_back_porch_us  = 1.50;
-
-const float hsync_us = hsync_front_porch_us + hsync_sync_tip_us + hsync_breezeway_us + hsync_color_burst_us + hsync_back_porch_us;
-
-const float hdata_us = scanline_us - hsync_us;
-
 float *scanline(unsigned char *); // [ 11us @ HSYNC ][ 52.5us @ f(t)V ]
 
 float *vblank_scanline(); // [ 11us @ HSYNC ][ 52.5us @ Vblk ]
 
-void usage(const char *prog) {
-}
+void init();
+void usage(const char *prog);
 
 int main(int argc, char **argv) {
 
 	exit(EXIT_SUCCESS);
+}
+
+void usage(const char *prog) {
+}
+
+
+
+void init() {
+	hsync_us = hsync_front_porch_us + hsync_sync_tip_us + hsync_breezeway_us + hsync_color_burst_us + hsync_back_porch_us;
+	hdata_us = scanline_us - hsync_us;
 }
 
